2454f6d44e4c2c265b8d9651cf4b6682ec0671ac
GuterKase <2916675@gmail.com>
2022-05-05 13:43:07 +0900

    Project created

---
+++ HackatonProject/HackatonProject/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using HackatonProject.Views;
+using Xamarin.Forms;
+
+namespace HackatonProject
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new HomeView();
+        }
+    }
+}
